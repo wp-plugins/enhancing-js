4dfe096ead3ec337a798fce5ea9923d872e4e999
miyauchi <miyauchi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-12 10:57:03 +0000

    first release
    git-svn-id: https://plugins.svn.wordpress.org/enhancing-js/trunk@288342 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ EnhancingJS.php
@@ -0,0 +1,232 @@
+<?php
+/*
+Plugin Name: Enhancing JS
+Plugin URI: http://firegoby.theta.ne.jp/wp/enhancingjs
+Description: Add & Edit custom JavaScript throught WordPress Dashboard with visual editor.
+Author: Takayuki Miyauchi (THETA NETWORKS Co,.Ltd)
+Version: 0.1
+Author URI: http://firegoby.theta.ne.jp/
+*/
+
+/*
+Copyright (c) 2010 Takayuki Miyauchi (THETA NETWORKS Co,.Ltd).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+require_once(dirname(__FILE__).'/addrewriterules.class.php');
+
+new EnhancingJS();
+
+
+class EnhancingJS{
+
+    private $title      = '';
+    private $name       = 'EnhancingJS';
+    private $role       = 'edit_theme_options';
+    private $basedir    = null;
+
+    function __construct()
+    {
+        new AddRewriteRules(
+            $this->name.'.js$',
+            $this->name,
+            array(&$this, 'get_js')
+        );
+        $this->basedir = WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__));
+        add_action('admin_menu', array(&$this, 'add_admin_page'));
+        add_action('admin_head', array(&$this, 'admin_head'));
+        add_action('wp_head', array(&$this, 'wp_head'));
+        add_action('wp_print_scripts', array(&$this, 'wp_print_scripts'));
+    }
+
+    public function wp_print_scripts()
+    {
+        if (!is_admin()) {
+            $scripts = $this->get_enqueue();
+            foreach ($scripts as $s) {
+                wp_enqueue_script($s);
+            }
+        }
+    }
+
+    private function get_js_url()
+    {
+        global $wp_rewrite;
+        if ($wp_rewrite->using_permalinks()) {
+            $url = site_url().'/'.$this->name.'.js';
+        } else {
+            $url = site_url().'/?'.$this->name.'=true';
+        }
+        return $url;
+    }
+
+    public function wp_head()
+    {
+        $css = "<script type=\"text/javascript\" src=\"%s\"></script>";
+        echo "<!-- Enhancing JS Plugin -->\n";
+        echo sprintf($css, $this->get_js_url())."\n";
+    }
+
+    public function get_js()
+    {
+        header('Content-type: text/javascript');
+        echo $this->get_js_src();
+        exit;
+    }
+
+    private function get_js_src()
+    {
+        if($js = trim(get_option('EnhancingJS'))){
+            $css = stripslashes($js);
+        } else {
+            $css = "/* {$this->title} */\n";
+        }
+        return $css;
+    }
+
+    public function add_admin_page(){
+        load_plugin_textdomain(
+            $this->name, 
+            PLUGINDIR.'/'.dirname(plugin_basename(__FILE__)).'/langs', 
+            dirname(plugin_basename(__FILE__)).'/langs'
+        );
+        $this->title = __('Enhancing JS', $this->name);
+        add_theme_page(
+            $this->title,
+            $this->title,
+            $this->role,
+            $this->name,
+            array(&$this, 'admin_page')
+        );
+    }
+
+    public function admin_head()
+    {
+        if (isset($_GET['page']) && $_GET['page'] == $this->name) {
+            $script = '<script src="%s" type="%s" charset="%s"></script>';
+            echo sprintf($script, $this->basedir.'/codemirror/codemirror.js', 'text/javascript', 'UTF-8')."\n";
+            echo sprintf($script, $this->basedir.'/js/jseditor.js', 'text/javascript', 'UTF-8')."\n";
+            $css = "<link rel=\"stylesheet\" href=\"%s\" type=\"text/css\" />";
+            echo sprintf($css, $this->basedir.'/css/style.css');
+        }
+    }
+
+    public function admin_page(){
+        global $wp_rewrite;
+
+        echo '<div class="wrap">';
+        echo '<form id="edit_css" action="'.$_SERVER['REQUEST_URI'].'" method="post">';
+        echo '<input type="hidden" id="action" name="action" value="save">';
+        echo '<div id="icon-themes" class="icon32"><br /></div>';
+
+        echo '<h2>'.$this->title.'</h2>';
+        if ( isset($_POST['action']) && $_POST['action'] == 'save' ){
+            $this->save_enqueue();
+            update_option('EnhancingJS', $_POST['EnhancingJS']);
+            echo "<div id=\"message\" class=\"updated fade\"><p><strong>".__("Saved.")."</strong></p></div>";
+        }
+
+        $url = $this->get_js_url();
+        echo "<p><a href=\"{$url}\">{$url}</a></p>";
+        echo '<div id="editor" class="stuffbox">';
+        echo '<textarea id="EnhancingJS" name="EnhancingJS" js="width:90%;height:300px;">';
+        echo $this->get_js_src();
+        echo '</textarea>';
+        echo '</div><!--end #editor-->';
+
+        echo '<p>';
+        echo '<p><input type="submit" class="button-primary" value="'.__('Save Changes').'" /></p>';
+        echo '</p>';
+        echo "<script type=\"text/javascript\">\n";
+        echo "  var obj = document.getElementById('EnhancingJS');\n";
+        echo "  var btn = [\n";
+        echo "    ['".__('Undo', $this->name)."', 'undo'],\n";
+        echo "    ['".__('Redo', $this->name)."', 'redo'],\n";
+        echo "    ['".__('Search', $this->name)."', 'search'],\n";
+        echo "    ['".__('Replace', $this->name)."', 'replace']\n";
+        echo "  ];\n";
+        echo "  new JSEditor('EnhancingJS', '".$this->basedir."', btn);";
+        echo "</script>";
+        $this->get_registered_scripts();
+        echo '<p><input type="submit" class="button-primary" value="'.__('Save Changes').'" /></p>';
+        echo '</form>';
+        echo "</wrap>";
+    }
+
+    private function get_registered_scripts()
+    {
+        $wp_scripts = new WP_Scripts();
+        wp_default_scripts($wp_scripts);
+
+        echo "<h3 style=\"margin-top:2em;\">Enqueue Scripts</h3>";
+        echo "<p>".__("Please check, if you want to load automatically.", $this->name)."</p>";
+        echo "<table class=\"widefat\" cellspacing=\"0\">";
+        echo "<thead>";
+        echo "<tr>";
+        echo "<th scope=\"col\" class=\"manage_column check-column\"><input type=\"checkbox\"></th>";
+        echo "<th scope=\"col\" class=\"manage_column\">".__('Handle', $this->name)."</th>";
+        echo "<th scope=\"col\" class=\"manage_column\">".__("Version", $this->name)."</th>";
+        echo "<th scope=\"col\" class=\"manage_column\">".__("Deps", $this->name)."</th>";
+        echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
+        sort($wp_scripts->registered);
+        $saved = $this->get_enqueue();
+        foreach ($wp_scripts->registered as $r) {
+            if (preg_match("/^\/wp-admin/", $r->src)) {
+                continue;
+            }
+            echo "<tr>";
+            if (in_array($r->handle, $saved)) {
+                echo "<th scope=\"row\" class=\"check-column\"><input type=\"checkbox\" name=\"load[]\" value=\"{$r->handle}\" checked=\"checked\" /></th>";
+            } else {
+                echo "<th scope=\"row\" class=\"check-column\"><input type=\"checkbox\" name=\"load[]\" value=\"{$r->handle}\" /></th>";
+            }
+            echo "<td>{$r->handle}<br />{$r->src}</td>";
+            echo "<td>{$r->ver}</td>";
+            $deps = join(", ", $r->deps);
+            echo "<td>{$deps}</td>";
+            echo "</tr>";
+        }
+        echo "</tbody>";
+        echo "</table>";
+    }
+
+    private function save_enqueue()
+    {
+        if (isset($_POST['load']) && is_array($_POST['load']) && $_POST['load']) {
+            update_option('EnhancingJS.load', $_POST['load']);
+        } else {
+            update_option('EnhancingJS.load', array());
+        }
+    }
+
+    private function get_enqueue()
+    {
+        $load = get_option('EnhancingJS.load');
+        if (is_array($load)) {
+            return $load;
+        } else {
+            return array();
+        }
+    }
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,79 @@
+=== Enhancing JavaScript ===
+Contributors: miyauchi
+Donate link: http://firegoby.theta.ne.jp/
+Tags: CSS, JavaScript, Visual Editor
+Requires at least: 3.0
+Tested up to: 3.0
+Stable tag: 0.1
+
+Enhancing JS plugin will enable to edit external JavaScript.
+In addition, this plugin implements simple visual editor for JavaScript.
+
+== Description ==
+
+Enhancing JS plugin will enable to edit external JavaScript.
+This JavaScript is output through WordPress rewrite API.
+In addition, this plugin implements simple visual editor for JavaScript.
+
+If you will install this plugin with ['Enhancing CSS' plugin](http://wordpress.org/extend/plugins/enhancing-css/),
+You can customize themes without PHP programming!
+
+* [Plugin Homepage](http://firegoby.theta.ne.jp/wp/enhancingjs)
+* [Support](http://wordpress.org/tags/enhancing-js)
+
+= Some features: =
+
+* You can edit External JavaScript on visual editor.
+* You can manage WordPress Default JavaScript frameworks.
+
+= Translators: =
+
+* Japanese(ja) - [Takayuki Miyauchi](http://firegoby.theta.ne.jp/)
+
+You can send your own language pack to me.
+
+
+== Installation ==
+
+* Upload the "Enhancing JS" plugin to your blog.
+* Activate the plugin through the 'Plugins' menu in WordPress.
+* Visit "Enhancing JS" in Appearance.
+
+== Frequently Asked Questions ==
+
+= This JavaScript is physical file? =
+No. This JavaScript output through WordPress rewrite API.
+
+= JavaScript path? =
+Example: http://your_site_url/EnhancingJS.js
+
+= Can I use "Enhancing JS" with prototype.js, jquery and other JavaScript frameworks? =
+Yes.
+Please check, if you want to load WordPress default javascripts on Admin Panel.
+
+= What are Capabilities? =
+edit_theme_options
+
+== Screenshots ==
+
+1. Simple visual editor.
+2. Manage enqueue scripts.
+
+== Changelog ==
+
+= 0.1 =
+
+* The first release.
+
+== Credits ==
+
+This plug-in is not guaranteed though the user of WordPress can freely use this plug-in free of charge regardless of the purpose.
+The author must acknowledge the thing that the operation guarantee and the support in this plug-in use are not done at all beforehand.
+
+WordPress のユーザーは目的を問わず、このプラグインを無償で自由に利用することができますがこのプラグインは無保証です。
+作者はこのプラグインの利用における一切の動作保証とサポートを行わない事を予めご了承下さい。
+
+== Contact ==
+
+email to miya[at]theta.ne.jp
+
